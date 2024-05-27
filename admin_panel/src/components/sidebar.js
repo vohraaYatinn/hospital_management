55f5f1b844d6343a0f239458d8d71f6903bd1d06
vohraaYatinn <vohrayatin9@gmail.com>
2024-05-27 21:12:05 +0530

    ok

--- admin_panel/src/components/sidebar.js
+++ admin_panel/src/components/sidebar.js
@@ -221,21 +221,21 @@ export default function Sidebar({ manuClass }) {
                 <li className={`${manu === "hospitals" ? "active" : ""} ms-0`}>
                   <Link to="/hospitals">Current Hospitals</Link>
                 </li>
-                <li
+                {/* <li
                   className={`${
                     manu === "add-hospital-admin" ? "active" : ""
                   } ms-0`}
                 >
                   <Link to="/add-hospital-admin">Add Hospital Admins</Link>
-                </li>
+                </li> */}
 
-                <li
+                {/* <li
                   className={`${
                     manu === "hospitals-admin" ? "active" : ""
                   } ms-0`}
                 >
                   <Link to="/hospitals-admin">All Hospital Account</Link>
-                </li>
+                </li> */}
               </ul>
             </div>
           </li>
