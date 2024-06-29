e57180bbe9f66af892ed93e1c9309c41e2b8c6e0
Yatin Vohra <96340665+vohraaYatinn@users.noreply.github.com>
2024-06-30 02:56:31 +0530

    ss

--- admin_panel/src/utils/convertToPdf.js
+++ admin_panel/src/utils/convertToPdf.js
@@ -1,10 +1,10 @@
 import html2pdf from 'html2pdf.js';
 
-const convertToPDF = (htmlContent, fileName) => {
+const convertToPDF = (htmlContent, fileName = "Prescription") => {
 
   const pdfOptions = {
     margin: 10,
-    filename: "Prescription",
+    filename: fileName,
     image: { type: 'jpeg', quality: 0.98 },
     html2canvas: { scale: 2 },
     jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
--- hospital_panel/src/page/InvoiceUjur.js
+++ hospital_panel/src/page/InvoiceUjur.js
@@ -35,6 +35,27 @@ const InvoiceModal = ({ show,setShow, invoice }) => {
         <br />
       </p>
     </div>
+  <div className="col-sm-6" style={{
+    display:"flex",
+    flexDirection:"column",
+    alignItems:"flex-end"
+  }}>
+  <img src={logoDark} alt="Hospital Logo" style={{
+    height:"3rem",
+    marginTop:"1rem"
+  }}/>
+
+      <p style={{
+        textAlign:"end",
+        fontSize:"0.8rem",
+        fontFamily:"inherit"
+      }}>
+      support@ujurcare.com
+        <br />
+        www.ujurcare.com
+        <br />
+      </p>
+    </div>
     <div className="col-sm-6 text-right">
       
     
@@ -47,18 +68,28 @@ const InvoiceModal = ({ show,setShow, invoice }) => {
       <p>
         {invoice?.appointment?.patient?.full_name}
         <br />
-        {invoice?.appointment?.patient?.block}
+        District: {invoice?.appointment?.patient?.district && invoice?.appointment?.patient?.district?.replace(/^./, str => str.toUpperCase())}
         <br />
-        {invoice?.appointment?.patient?.district}
+        {invoice?.appointment?.patient?.user?.email}
+        <br />
+        <span style={{
+          fontWeight:800
+        }}>{invoice?.appointment?.patient?.user?.phone}</span>
+        
       </p>
     </div>
-    <div className="col-sm-6 text-right">
-      <h5>Appointment Details:</h5>
-      <p>
-        Appointment ID: {invoice?.appointment?.id}
+    <div className="col-sm-6 text-right" >
+      <h5 style={{
+        textAlign:"end"
+      }}>Appointment Details:</h5>
+      <p style={{
+        textAlign:"end"
+      }}>
+        Hospital: {invoice?.appointment?.doctor?.hospital?.name}
         <br />
         Booking Date: {moment(invoice?.appointment?.date_appointment).format("dddd, MMM D, YYYY")}
-
+        <br/>
+        Visit: {invoice?.appointment?.slot}
         
       </p>
     </div>
@@ -68,27 +99,25 @@ const InvoiceModal = ({ show,setShow, invoice }) => {
       <table className="table table-bordered">
         <thead className="thead-light">
           <tr>
-            <th>Description</th>
-            <th>Doctor fees</th>
+            <th>Item</th>
+            <th>Consultation fees</th>
             <th>Booking charges</th>
+            <th>Total</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Consultation Fee<br/>({invoice?.hospital})</td>
-            <td>Rs {(parseFloat(invoice?.items[0]?.quantity) - parseFloat(invoice?.items[0]?.quantity)*0.18).toFixed(2)}</td>
-            <td>Rs {(parseFloat(invoice?.items[0]?.price) - parseFloat(invoice?.items[0]?.price*0.18)).toFixed(2)}</td>
+            <td><span style={{
+              fontWeight:600
+            }}>{invoice?.appointment?.doctor?.full_name}</span><br/>({invoice?.hospital})</td>
+            <td>Rs {invoice?.items[0]?.quantity}</td>
+            <td>Rs {invoice?.items[0]?.price}</td>
+            <th>Rs {(parseFloat(invoice?.items[0]?.quantity) + parseFloat(invoice?.items[0]?.price)).toFixed(2)}</th>
           </tr>
 
         </tbody>
         <tfoot>
-     
-          <tr>
-            <th colSpan={3} className="text-right">
-              Tax (18%)
-            </th>
-            <th>Rs {((parseFloat(invoice?.items[0]?.quantity) + parseFloat(invoice?.items[0]?.price )) * 0.18 ).toFixed(2)}</th>
-          </tr>
+   
           <tr>
             <th colSpan={3} className="text-right">
               Total
@@ -104,7 +133,6 @@ const InvoiceModal = ({ show,setShow, invoice }) => {
       <p>Thank you for using UJUR!</p>
     </div>
   </div>
-  <img src={logoDark} alt="Hospital Logo" class="watermark"/>
 
 </div>
 }
