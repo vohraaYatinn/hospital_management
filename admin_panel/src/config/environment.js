ff00cf56b3e0f511c2b3939268273b19ccb8c6ae
Yatin Vohra <96340665+vohraaYatinn@users.noreply.github.com>
2024-07-06 19:13:47 +0530

    ok

--- admin_panel/src/config/environment.js
+++ admin_panel/src/config/environment.js
@@ -1,6 +1,6 @@
 // /* eslint-disable */
-export const test_url = 'http://91.108.110.30:8000/';
-export const test_url_images = 'http://91.108.110.30:8000';
+export const test_url = 'http://api.chermulgold.com:8000/';
+export const test_url_images = 'http://api.chermulgold.com:8000';
 
 // export const test_url = 'http://127.0.0.1:8000/';
 // export const test_url_images = 'http://127.0.0.1:8000';
